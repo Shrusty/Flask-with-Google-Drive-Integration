abf55419b17c831ce1d6da814f8a930cff905b26
Shrusty <shrustyraj216@gmai.com>
2025-09-15 18:27:54 +0530

    Stop tracking temp_venv

--- temp_venv/Scripts/check_version.py
+++
@@ -1,5 +0,0 @@
-import pkg_resources
-print(pkg_resources.get_distribution("google-api-python-client").version)
-
-import googleapiclient.discovery
-print("Google API Client imported successfully!")
